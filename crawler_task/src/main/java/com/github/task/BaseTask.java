4fd3f501d656983aa44441b66b050d2a0c134c98
xubenqing_123 <xubenqing@enjoyk.cn>
2021-03-26 17:19:08 +0800

    edit3

--- crawler_task/src/main/java/com/github/task/BaseTask.java
+++ crawler_task/src/main/java/com/github/task/BaseTask.java
@@ -17,6 +17,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
